2e512a81985d616c751a34776580214fb03732f8
lot8229 <lot8229@kakao.com>
2021-01-13 20:44:51 +0900

    add bindAddress.c

--- bindAddress.c
+++ bindAddress.c
@@ -1,3 +1,6 @@
 //소켓의 주소 할당 및 연결 
 // 전화기에 전화번호가 부여되듯이 소켓에도 주소정보가 할당 된다.
 // 소켓의 주소정보는 IP 와 PORT 번호로 구성이 된다.
+#include <sys/socket.h>
+int bind (int sockfd, struct sockaddr *myaddr, socklen_t addrlen);
+// 성공시 0 , 실패시 -1 반환 
